100d9d052fbc3339ab51ced076374dca0d31abd4
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-23 21:10:41 +0530

    Added Alphabetics Vowel or Consonant program

--- BasicCoreProgramsDay-5/Program.cs
+++ BasicCoreProgramsDay-5/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome! to basic core programming");
             Console.WriteLine("Please choose any one program from below options.");         //User input for options
-            Console.WriteLine("1:FlipCoin\n2:LeapYear\n3:PowerOfTwo\n4:Harmonics\n5:Factors\n6:QuotientAndRemainder\n7:SwapTwoNumbers\n8:EvenOddNumber");
+            Console.WriteLine("1:FlipCoin\n2:LeapYear\n3:PowerOfTwo\n4:Harmonics\n5:Factors\n6:QuotientAndRemainder\n7:SwapTwoNumbers\n8:EvenOddNumber\n9:VowelConsonant");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)         //Calling different methods using switch case
             {
@@ -35,6 +35,9 @@
                     break;
                 case 8:
                     EvenOdd.EvenOddNumber();
+                    break;
+                case 9:
+                    VowelConsonant.vowelConsonant();
                     break;
                 default:
                     Console.WriteLine("Please enter correct option");
---
+++ BasicCoreProgramsDay-5/VowelConsonant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramsDay_5
+{
+    internal class VowelConsonant
+    {
+        public static void vowelConsonant()         //Creating method
+        {
+            Console.WriteLine("Plaese provide input string");       //Taking user input
+            string input = Console.ReadLine();
+            for (int i = 0; i <= input.Length; i++)       //For loop for condition 
+            {
+                if ((input[i] >= 'a' && input[i] <= 'z') || (input[i] >= 'A' && input[i] <= 'Z'))
+                {
+                    if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u' || input[i] == 'A' || input[i] == 'E' || input[i] == 'I' || input[i] == 'O' || input[i] == 'U')
+                        Console.WriteLine($"{input[i]} is vowel");
+                    else
+                        Console.WriteLine($"{input[i]} is consonant");
+                }
+            }
+        }
+    }
+}
