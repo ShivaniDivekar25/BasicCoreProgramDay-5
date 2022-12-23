43821e0838fea9a5fb8397fa3319504c853c0cee
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-23 19:25:49 +0530

    Added Welcome to Basic Core Programming

---
+++ BasicCoreProgramsDay-5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicCoreProgramsDay_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! to basic core programming");
+        }
+    }
+}
