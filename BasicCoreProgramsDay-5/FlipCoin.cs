c398d299b1b2c66c583c04dd780bae9d29b8cc9e
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-23 19:32:43 +0530

    Added Flip Coin Prgram

---
+++ BasicCoreProgramsDay-5/FlipCoin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramsDay_5
+{
+    internal class FlipCoin
+    {
+        public static void FlipCoin1()      //Method created
+        {
+            int tailCount = 0, headCount = 0;       //Initialise values
+            Console.WriteLine("Number of times to the flipcoin");       //Getting  number of count head and tail
+            int n = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();       //Random object is created
+
+            for (int i = 1; i <= n; i++)        //For loop for given condition
+            {
+                double input = random.NextDouble();     //Using random function
+                if (input < 0.5)        //Providing some condition statements
+                    tailCount++;
+                else
+                    headCount++;
+            }
+            Console.WriteLine("TailCount:{0} and HeadCount:{1}", tailCount, headCount);
+            double tailPercentage = (tailCount * 100) / n;      //Percentage calculating 
+            double headPercentage = (headCount * 100) / n;      //percentage calculating
+            Console.WriteLine("Tail={0}% and Head={1}%", tailPercentage, headPercentage);
+        }
+    }
+}
--- BasicCoreProgramsDay-5/Program.cs
+++ BasicCoreProgramsDay-5/Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome! to basic core programming");
+            Console.WriteLine("Please choose any one program from below options.");         //User input for options
+            Console.WriteLine("1:FlipCoin");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FlipCoin.FlipCoin1();       //Calling method
+                    break;
+                default:
+                    Console.WriteLine("Please enter correct option");
+                    break;
+            }
         }
     }
 }
