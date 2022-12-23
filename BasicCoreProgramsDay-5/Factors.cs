93422180694c75df0d7455d84b4601278d44f3ee
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-23 20:28:12 +0530

    Adding Factors Program

---
+++ BasicCoreProgramsDay-5/Factors.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramsDay_5
+{
+    internal class Factors
+    {
+        public static void factorsNumber(int input)         //Creating method
+        {
+            int Count = 0;      //Initializing value
+            for (int i = 2; i < input; i++)         //For loop to check condition
+            {
+                if (input % i == 0)
+                {
+                    Count = 1;
+                    Console.WriteLine($"{i} is factors of {input}");
+                }
+                for (int j = 2; j <= i - 1; j++)          //For loop to check condition
+                {
+                    if (i % j == 0)
+                    {
+                        Count = 0;
+                    }
+                }
+                if (Count == 1)
+                {
+                    Console.WriteLine("{0} is prime factor", i);
+                }
+            }
+        }
+    }
+}
--- BasicCoreProgramsDay-5/Program.cs
+++ BasicCoreProgramsDay-5/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome! to basic core programming");
             Console.WriteLine("Please choose any one program from below options.");         //User input for options
-            Console.WriteLine("1:FlipCoin\n2:LeapYear\n3:PowerOfTwo\n4:Harmonics");
+            Console.WriteLine("1:FlipCoin\n2:LeapYear\n3:PowerOfTwo\n4:Harmonics\n5:Factors");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -21,6 +21,11 @@
                     break;
                 case 4:
                     Harmonics.HarmonicNumber();
+                    break;
+                case 5:
+                    Console.WriteLine("Please enter input value");
+                    int input1 = Convert.ToInt32(Console.ReadLine());
+                    Factors.factorsNumber(input1);
                     break;
                 default:
                     Console.WriteLine("Please enter correct option");
