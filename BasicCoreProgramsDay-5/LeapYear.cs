21946e2b21f7348f830f3d09e08e5ae0a2ed20bf
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-23 19:42:44 +0530

    Added Leap Year Prgram

---
+++ BasicCoreProgramsDay-5/LeapYear.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramsDay_5
+{
+    internal class LeapYear
+    {
+        public static void LeapYearPrgoram()       //Created method
+        {
+            Console.WriteLine("Please enter year");         //Getting user input
+            int Year = Convert.ToInt32(Console.ReadLine());
+
+            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))         //Giving conditions to check
+                Console.WriteLine("The {0} is leap year", Year);
+            else
+                Console.WriteLine("The {0} is not leap year", Year);
+        }
+    }
+}
--- BasicCoreProgramsDay-5/Program.cs
+++ BasicCoreProgramsDay-5/Program.cs
@@ -6,12 +6,15 @@
         {
             Console.WriteLine("Welcome! to basic core programming");
             Console.WriteLine("Please choose any one program from below options.");         //User input for options
-            Console.WriteLine("1:FlipCoin");
+            Console.WriteLine("1:FlipCoin\n2:LeapYear");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     FlipCoin.FlipCoin1();       //Calling method
+                    break;
+                case 2:
+                    LeapYear.LeapYearPrgoram();
                     break;
                 default:
                     Console.WriteLine("Please enter correct option");
