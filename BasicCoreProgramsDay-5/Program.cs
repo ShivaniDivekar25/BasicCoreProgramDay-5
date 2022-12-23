140a90b6f4f16db7361298f56f170558ceb7e254
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-23 20:49:34 +0530

    Added Swap Two Numbers program

--- BasicCoreProgramsDay-5/Program.cs
+++ BasicCoreProgramsDay-5/Program.cs
@@ -6,12 +6,12 @@
         {
             Console.WriteLine("Welcome! to basic core programming");
             Console.WriteLine("Please choose any one program from below options.");         //User input for options
-            Console.WriteLine("1:FlipCoin\n2:LeapYear\n3:PowerOfTwo\n4:Harmonics\n5:Factors\n6:QuotientAndRemainder");
+            Console.WriteLine("1:FlipCoin\n2:LeapYear\n3:PowerOfTwo\n4:Harmonics\n5:Factors\n6:QuotientAndRemainder\n7:SwapTwoNumbers");
             int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
+            switch (option)         //Calling different methods using switch case
             {
                 case 1:
-                    FlipCoin.FlipCoin1();       //Calling method
+                    FlipCoin.FlipCoin1();       
                     break;
                 case 2:
                     LeapYear.LeapYearPrgoram();
@@ -29,6 +29,9 @@
                     break;
                 case 6:
                     QuotientAndRemainder.findingQuotientNumber();
+                    break;
+                case 7:
+                    SwapTwoNumbers.SwapTwoNumbers1();
                     break;
                 default:
                     Console.WriteLine("Please enter correct option");
---
+++ BasicCoreProgramsDay-5/SwapTwoNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramsDay_5
+{
+    internal class SwapTwoNumbers
+    {
+        public static void SwapTwoNumbers1()        //Creating method
+        {
+            int a = 5;          //Initializing input value
+            int b = 10;
+            int temp = a;
+            Console.WriteLine("Numbers before swaping is {0} and {1}", a, b);
+            a = b;
+            b = temp;
+            //Another method to solve
+            //a = a + b;
+            // b = a - b;
+            //a = a - b;
+            Console.WriteLine("Numbers after swaping is {0} and {1}", a, b);
+        }
+    }
+}
